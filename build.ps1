52b3bb309a3dbc9e4308d2e9fba7ba680a476233
Michael Dietrich <dev.michaeldietrich@outlook.de>
2019-11-25 14:14:57 +0100

    chore: Update NuGet naming in build script

--- build.ps1
+++ build.ps1
@@ -1,1 +1,60 @@
-dotnet pack src\SmartMvvm.Xaml\SmartMvvm.Xaml.csproj -p:VersionSuffix=pre0001 -o artifacts -c Release
+function Get-CurrentBranch
+{
+    $branch = $env:APPVEYOR_REPO_BRANCH
+    if (!$branch)
+    {
+        $branch = git rev-parse --abbrev-ref HEAD
+    }
+
+    if (!$branch)
+    {
+        $branch = 'unknown';
+    }
+
+    return $branch
+}
+
+function Get-VersionSuffix
+{
+    param( [string] $branch )
+
+    $buildNumber = $env:APPVEYOR_BUILD_NUMBER
+
+    if (!$buildNumber)
+    {
+        $buildNumber = 0
+    }
+
+    $buildNumber = "{0:d4}" -f $buildNumber
+
+    if ($branch -eq 'master')
+    {
+        return ''
+    }
+
+    if ($branch -eq 'develop')
+    {
+        return "alpha$buildNumber"
+    }
+
+    if ($branch.StartsWith('release/'))
+    {
+        return "beta$buildNumber"
+    }
+
+    $branchEscaped = $branch.Replace('/', '-').Replace('_', '-')
+
+    return "$branchEscaped$buildNumber"
+}
+
+$branch = Get-CurrentBranch
+$suffix = Get-VersionSuffix $branch
+
+$props = @('src\SmartMvvm.Xaml\SmartMvvm.Xaml.csproj', '-o', 'artifacts', '-c', 'Release')
+
+if ($suffix)
+{
+    $props = $props += "-p:VersionSuffix=$suffix"
+}
+
+dotnet pack $props
