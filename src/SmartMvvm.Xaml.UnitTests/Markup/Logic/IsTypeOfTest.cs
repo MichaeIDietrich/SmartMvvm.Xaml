2301ba8ca6d31ce49dc17c62684d2a9e373a6bcd
Felix Löschau <floeschau@loeschausoft.com>
2021-11-25 21:52:47 +0100

    feat: moved type check to new markup extensions

---
+++ src/SmartMvvm.Xaml.UnitTests/Markup/Logic/IsTypeOfTest.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Data;
+using SmartMvvm.Xaml.Markup.Logic;
+using Xunit;
+
+namespace SmartMvvm.Xaml.UnitTests.Markup.Logic
+{
+    public sealed class IsTypeOfTest
+    {
+        [Theory]
+        [MemberData(nameof(ParametersData))]
+        public void Check_Whether_First_Is_Equal_To_Second(object param1, object param2, bool expected)
+        {
+            // given
+            var sut = new Equal(param1, param2);
+
+            // when
+            var result = Evaluator.Evaluate(sut);
+
+            // then
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Use_FallbackValue_On_Error()
+        {
+            // given
+            var sut = new Equal(true, 'a') { Epsilon = 0, FallbackValue = "FALLBACK" };
+
+            // when
+            var result = Evaluator.Evaluate(sut);
+
+            // then
+            Assert.Equal("FALLBACK", result);
+        }
+
+        public static IEnumerable<object[]> ParametersData()
+        {
+            // left param, right param, expected result
+            yield return new object[] { "123", typeof(string), true };
+            yield return new object[] { 5, typeof(int), true };
+            yield return new object[] { 5.6d, typeof(double), true };
+            yield return new object[] { 4, typeof(string), false };
+            yield return new object[] { 3.45d, typeof(int), false };
+            yield return new object[] { "123", typeof(double), false };
+            yield return new object[] { new Binding { Source = "123" }, typeof(string), true };
+            yield return new object[] { new Binding { Source = 5 }, typeof(int), true };
+            yield return new object[] { new Binding { Source = 5.6d }, typeof(double), true };
+            yield return new object[] { new Binding { Source = 4 }, typeof(string), false };
+            yield return new object[] { new Binding { Source = 3.45d }, typeof(int), false };
+            yield return new object[] { new Binding { Source = "123" }, typeof(double), false };
+            yield return new[] { DependencyProperty.UnsetValue, "155", false };
+            yield return new[] { DependencyProperty.UnsetValue, DependencyProperty.UnsetValue, true };
+        }
+    }
+}
---
+++ src/SmartMvvm.Xaml/Markup/Logic/IsTypeOf.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace SmartMvvm.Xaml.Markup.Logic
+{
+    /// <summary>
+    ///     Represents a markup extension that checks if a given object is equal to a given type.
+    /// </summary>
+    public class IsTypeOf : LogicalBase
+    {
+        /// <summary>
+        ///     Initializes a new instance of <see cref="IsTypeOf" />.
+        /// </summary>
+        /// <param name="left">Input value.</param>
+        /// <param name="right">type.</param>
+        public IsTypeOf(object left, Type right)
+            : base(left, right, null)
+        {
+        }
+
+        /// <summary>
+        ///     Gets or sets a value that is used as return value if comparing fails.
+        /// </summary>
+        public object FallbackValue { get; set; } = DependencyProperty.UnsetValue;
+
+        /// <InheritDoc />
+        protected override object Evaluate(IReadOnlyList<object> values)
+        {
+            try
+            {
+                if (values[0] != null && values[1] is Type)
+                    return ReferenceEquals(values[0].GetType(), values[1]);
+
+                return false;
+            }
+            catch when (FallbackValue != DependencyProperty.UnsetValue)
+            {
+                return FallbackValue;
+            }
+        }
+    }
+}
