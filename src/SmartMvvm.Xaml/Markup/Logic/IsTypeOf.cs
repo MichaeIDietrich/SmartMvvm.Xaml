ef5055b6405539cd6ba844da890d77df8ad6727d
Felix Löschau <floeschau@loeschausoft.com>
2022-01-15 09:21:55 +0100

    style: applied comment code style

--- src/SmartMvvm.Xaml/Markup/Logic/IsTypeOf.cs
+++ src/SmartMvvm.Xaml/Markup/Logic/IsTypeOf.cs
@@ -5,22 +5,21 @@ using System.Windows;
 namespace SmartMvvm.Xaml.Markup.Logic
 {
     /// <summary>
-    ///     Represents a markup extension that checks if a given object is equal to a given type.
+    /// Represents a markup extension that checks if a given object is equal to a given type.
     /// </summary>
     public class IsTypeOf : LogicalBase
     {
         /// <summary>
-        ///     Initializes a new instance of <see cref="IsTypeOf" />.
+        /// Initializes a new instance of <see cref="IsTypeOf"/>.
         /// </summary>
         /// <param name="left">Input value.</param>
         /// <param name="right">type.</param>
         public IsTypeOf(object left, Type right)
             : base(left, right, null)
-        {
-        }
+        { }
 
         /// <summary>
-        ///     Gets or sets a value that is used as return value if comparing fails.
+        /// Gets or sets a value that is used as return value if comparing fails.
         /// </summary>
         public object FallbackValue { get; set; } = DependencyProperty.UnsetValue;
 
